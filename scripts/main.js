fb41d3c1a9620439fe728fb836c4b692c592a88f
Theo-FACCIOTTI <theo.facciotti@etu.univ-amu.fr>
2022-02-28 12:04:02 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,15 @@
-
 function sideMenu() {
     let sideNav = document.getElementById('side-nav');
     if (sideNav.style.display == 'none') {
         sideNav.style.display = 'block';
-    }
-    else {
+    } else {
         sideNav.style.display = 'none';
     }
 }
 
 
 Array.from(document.querySelectorAll('#nav-onglet ul li a')).forEach(function(onglet) {
-    onglet.addEventListener('click', 
+    onglet.addEventListener('click',
         function(event) {
             Array.from(document.querySelectorAll('.active')).forEach(function(elem_active) {
                 elem_active.removeAttribute('class');
@@ -23,7 +21,13 @@ Array.from(document.querySelectorAll('#nav-onglet ul li a')).forEach(function(on
 });
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/my-custom-pwa/sw.js') // à adapter à l'URL du projet
-        .then(() => { console.log('Service Worker Registered'); });
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+            // Register fonctionne
+            console.log('Service Worker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // Register ne marche pas
+            console.log('Service Worker registration failed: ', err);
+        });
+    });
 }
