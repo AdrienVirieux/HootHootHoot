4cf64173ee5ed8833040fb1737119783167290bb
Patate_Douce <72559937+AdrienVirieux@users.noreply.github.com>
2022-03-12 21:12:34 +0100

    Pour test l'import de fichier js

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,31 +1,33 @@
-var xyValues = [
-  {x:50, y:7},
-  {x:60, y:8},
-  {x:70, y:8},
-  {x:80, y:9},
-  {x:90, y:9},
-  {x:100, y:9},
-  {x:110, y:10},
-  {x:120, y:11},
-  {x:130, y:14},
-  {x:140, y:14},
-  {x:150, y:15}
-];
 
-new Chart("myChart", {
-  type: "scatter",
-  data: {
-    datasets: [{
-      pointRadius: 4,
-      pointBackgroundColor: "rgb(0,0,255)",
-      data: xyValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    scales: {
-      xAxes: [{ticks: {min: 40, max:160}}],
-      yAxes: [{ticks: {min: 6, max:16}}],
+
+function graph() {
+  let arraysJSON = JSON.parse(localStorage.getItem('TEMPERATURE'));
+
+  new Chart("myChart", {
+    type: 'line',
+    data: {
+      datasets: [{
+        data: [{
+          x: '2021-11-06 23:39:30',
+          y: 50
+        }, {
+          x: '2021-11-07 01:00:28',
+          y: 60
+        }, {
+          x: '2021-11-07 09:00:28',
+          y: 20
+        }]
+      }],
+    },
+    options: {
+      scales: {
+        x: {
+          min: '2021-11-07 00:00:00',
+        }
+      }
     }
-  }
-});
+  });
+}
+
+
+export { graph };
