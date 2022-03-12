2417b7526b2f9589d491bc54d62a145e9d683605
Theo-FACCIOTTI <theo.facciotti@etu.univ-amu.fr>
2022-03-12 10:45:36 +0100

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-
 var CACHE_NAME = '_HotHotHot_offline';
 var urlCache = [
     "/",
@@ -21,122 +20,95 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-    caches.match(event.request)
+        caches.match(event.request)
         .then(function(response) {
-        // Cache Trouvé - return la réponse
-        if (response) {
-            return response;
-        }
-
-        return fetch(event.request).then(
-            function(response) {
-            // On check si la réponse est valide
-            if(!response || response.status !== 200 || response.type !== 'basic') {
+            // Cache Trouvé - return la réponse
+            if (response) {
                 return response;
             }
 
-            // On clone la réponse pour la mettre en cache et l'afficher dans le browser
-            var responseToCache = response.clone();
+            return fetch(event.request).then(
+                function(response) {
+                    // On check si la réponse est valide
+                    if (!response || response.type !== 'basic') {
+                        return response;
+                    }
 
-            caches.open(CACHE_NAME)
-                .then(function(cache) {
-                cache.put(event.request, responseToCache);
-                });
+                    // On clone la réponse pour la mettre en cache et l'afficher dans le browser
+                    var responseToCache = response.clone();
 
-            return response;
-            }
-        );
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
+            );
         })
     );
 });
 
-// Récupère les ressources depuis le serveur
-const fromNetwork = (request, timeout) => new Promise((fulfill, reject) => {
-    const timeoutId = setTimeout(reject, timeout);
-    fetch(request).then(response => {
-      clearTimeout(timeoutId);
-      fulfill(response);
-      update(request);
-    }, reject);
-});
-
-// récupère les ressources depuis le cache
-const fromCache = request => caches.open(CURRENT_CACHE).then(cache => cache.match(request).then(matching => matching));
-
-// met en cache la page
-const updateCache = request => caches.open(CURRENT_CACHE).then(cache =>fetch(request).then(response => cache.put(request, response)));
-
-// Serveur First
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fromNetwork(event.request, 10000).catch(() => fromCache(event.request))
-  );
-  event.waitUntil(updateCache(event.request));
-});
-
-// Récupérer les ressources depuis le serveur, si réussi on les mets en cache.
-// Afficher depuis le cache.
-
 // -----------------------------------------------
 // Notifications
-function randomNotification() {  
-    var randomNumber = getRandomInt(5);  
-    console.log(randomNumber);  
-    if(randomNumber >= 2) {  
-        var notifTitle = "Chaud, non ?";  
-        var notifBody = 'Température : ' + randomNumber + '.';  
-        var notifImg = '/assets/images/android-chrome-192x192.png';  
-        var options = {  
-                    body: notifBody,  
-        icon: notifImg  
-            }  
-        var notif = new Notification(notifTitle, options);  
-        }  
-        setTimeout(randomNotification, 180000);  
-}  
-     
-//On génére un nombre aléatoire pour la démo  
-function getRandomInt(max) {  
-    return Math.floor(Math.random() * Math.floor(max));  
+function randomNotification() {
+    var randomNumber = getRandomInt(5);
+    console.log(randomNumber);
+    if (randomNumber >= 2) {
+        var notifTitle = "Chaud, non ?";
+        var notifBody = 'Température : ' + randomNumber + '.';
+        var notifImg = '/assets/images/android-chrome-192x192.png';
+        var options = {
+            body: notifBody,
+            icon: notifImg
+        }
+        var notif = new Notification(notifTitle, options);
+    }
+    setTimeout(randomNotification, 180000);
 }
 
-var button = document.getElementById("notifications");  
-button.addEventListener('click', function(e) {  
-    Notification.requestPermission().then(function(result) {  
-        if(result === 'granted') {  
-            randomNotification();  
-  }  
-    });  
+//On génére un nombre aléatoire pour la démo  
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+var button = document.getElementById("notifications");
+button.addEventListener('click', function(e) {
+    Notification.requestPermission().then(function(result) {
+        if (result === 'granted') {
+            randomNotification();
+        }
+    });
 });
 
 
 // -----------------------------------------------
 // bouton d'installation
-let deferredPrompt;  
-const addBtn = document.querySelector('.add-button');  
-addBtn.style.display = 'block';  
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'block';
 
-window.addEventListener('beforeinstallprompt', (e) => {  
+window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt  
-    e.preventDefault();  
+    e.preventDefault();
     // Stash the event so it can be triggered later.  
-    deferredPrompt = e;  
+    deferredPrompt = e;
     // Update UI to notify the user they can add to home screen  
-    addBtn.style.display = 'block';  
+    addBtn.style.display = 'block';
 
-    addBtn.addEventListener('click', (e) => {  
+    addBtn.addEventListener('click', (e) => {
         // hide our user interface that shows our A2HS button  
-        addBtn.style.display = 'none';  
+        addBtn.style.display = 'none';
         // Show the prompt  
-        deferredPrompt.prompt();  
+        deferredPrompt.prompt();
         // Wait for the user to respond to the prompt  
-        deferredPrompt.userChoice.then((choiceResult) => {  
-            if (choiceResult.outcome === 'accepted') {  
-                console.log('User accepted the A2HS prompt');  
-            } else {  
-                console.log('User dismissed the A2HS prompt');  
-            }  
-            deferredPrompt = null;  
-        });  
-    });  
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
 });
